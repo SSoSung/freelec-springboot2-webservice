a85fab434f7c2dce8014d88f3bb78a3fa48d8da7
김현성 <gustjd1209@naver.com>
2022-03-26 16:55:51 +0900

    version 맞춤

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야만 한다.
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // 내장 WAS를 실행한다. 서버에 Tomcat을 설치할 필요가 없게 되고, 스프링부트로 만들어진 Jar 파일로 실행
+    }
+}
